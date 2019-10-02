ac950099a4c84b737a705d205c9750a4c9195aa7
Riad Bensalem <fr_bensalem@esi.dz>
2019-10-02 23:53:52 +0200

    add tab navigation to favorites

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,9 +1,15 @@
 // Navigation/Navigation.js
+import React from 'react';
+import { StyleSheet,Text,Image } from 'react-native'
 
-import { createAppContainer, } from 'react-navigation'
+import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 import Search from '../components/Search'
 import FilmDetail from '../components/FilmDetail'
+import Favorites from '../components/Favorites'
+
+
 
 const SearchStackNavigator = createStackNavigator({
   Search: {
@@ -12,14 +18,49 @@ const SearchStackNavigator = createStackNavigator({
       title: 'Rechercher'
     }
   },
-FilmDetail:{
-    screen: FilmDetail,
-    navigationOptions: {
-      title: 'Detail du film'
-    }
+  FilmDetail: {
+    screen: FilmDetail
   }
-
-
 })
 
-export default createAppContainer(SearchStackNavigator)
+const MoviesTabNavigator = createBottomTabNavigator(
+  {
+    Search: {
+      screen: SearchStackNavigator,
+      navigationOptions: {
+        tabBarIcon: () => { // On définit le rendu de nos icônes par les images récemment ajoutés au projet
+          return <Image
+            source={require('../Images/ic_search.png')}
+            style={styles.icon}/> // On applique un style pour les redimensionner comme il faut
+        }
+      }
+    },
+    Favorites: {
+      screen: Favorites,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return <Image
+            source={require('../Images/1.png')}
+            style={styles.icon}/>
+        }
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeBackgroundColor: '#DDDDDD', // Couleur d'arrière-plan de l'onglet sélectionné
+      inactiveBackgroundColor: '#FFFFFF', // Couleur d'arrière-plan des onglets non sélectionnés
+      showLabel: false, // On masque les titres
+      showIcon: true // On informe le TabNavigator qu'on souhaite afficher les icônes définis
+    }
+  }
+)
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  }
+})
+
+export default createAppContainer(MoviesTabNavigator)
---
+++ components/Favorites.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {StyleSheet,View,Text} from 'react-native'
+
+class Favorites extends React.Component{
+  render(){
+    return(
+      <Text>hello </Text>
+    )
+  }
+}
+const styles=StyleSheet.create({
+
+
+})
+export default Favorites
